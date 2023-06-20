7b25ccfc0a690d0bace04f64be87243566ddf450
nishedh <restricted.karki@gmail.com>
2023-06-20 18:08:03 +0545

    cleanups in backend

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,9 @@ dotenv.config();
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const access_token = "";
-//   "AQArKn2l0LcDbWCFIM4AnfrHIhoIztUf2R2XoXe1vWAy-L-9gf_EJAhyai5Y348mIUhyvyTl_xhrLfwxaOPnDmzCIzY_7d96ANwwuFxKmeCU3CPwhAaQC73egeUh09n1l6l4vgcuXagLS339DQNgwtlveurueh2OOzJAe33wZz10mKFeteFIOf-9ABeGACWws9RtFGsAgiHjMHULvxCdyJ-vroCjZ0tHdqk";
 
 var token;
 const scope = "playlist-modify-private playlist-modify-public";
-var state = "WqZuWSpofmud4SMn";
-// getPlaylist("8d459e9cb093758b9c7153599379aa10");
-// getPlaylist("");
 app.use("/api/", async (req, res, next) => {
   var code;
   console.log("HERE");
@@ -37,27 +33,6 @@ app.use("/api/", async (req, res, next) => {
   req.token = token;
   next();
   return;
-  fs.readFile("config.JSON", "utf-8", async (err, data) => {
-    if (err) {
-      console.log("error reading file!");
-      redirect("localhost:8001/login");
-    } else {
-      console.log(jsondata);
-      fs.writeFile(
-        "config.json",
-        JSON.stringify({ code: code, token: token }),
-        "utf-8",
-        async (err, data) => {
-          console.log("HERE!!!");
-          if (err) console.log(err);
-          console.log("Writing data into the file!");
-          console.log(data);
-        }
-      );
-      req.token = token;
-      next();
-    }
-  });
 });
 
 app.use("/api/", api);
